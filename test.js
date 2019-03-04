d2572726c6bed7481a9d4a6100d2d5f0ae1d8305
Vadim Demedes <vdemedes@gmail.com>
2019-03-04 00:13:55 -0800

    Use Ink 2.0.0

--- test.js
+++ test.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import {serial as test} from 'ava';
-import {renderToString, Color} from 'ink';
+import {render, Color} from 'ink';
 import clearModule from 'clear-module';
 import stripAnsi from 'strip-ansi';
+
+// Fake process.stdout
+class Stream {
+	constructor() {
+		this.output = '';
+		this.columns = 100;
+	}
+
+	write(str) {
+		this.output = str;
+	}
+
+	get() {
+		return this.output;
+	}
+}
+
+const renderToString = node => {
+	const stream = new Stream();
+
+	render(node, {
+		stdout: stream,
+		debug: true
+	});
+
+	return stream.get();
+};
 
 test('render', t => {
 	// TODO: Find out why this doesn't work to prevent color
